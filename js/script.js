086b0a179475071cd9cda80487013fbc24c20694
Abdulsalam <abdulsalamabodunrin369@gmail.com>
2020-07-23 13:53:44 +0100

    fixed overflow

--- js/script.js
+++ js/script.js
@@ -1,10 +1,44 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToScroll: 1,
-    slidesToShow: 5.5,
-    draggable: true,
-    dots: '.dots',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
+ScrollReveal().reveal('.header')
+ScrollReveal().reveal('.categories')
+ScrollReveal().reveal('.adventures')
+ScrollReveal().reveal('.video-display')
+ScrollReveal().reveal('.footer');
+
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  nextArrow: null,
+  prevArrow:null,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
     }
-  });
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
